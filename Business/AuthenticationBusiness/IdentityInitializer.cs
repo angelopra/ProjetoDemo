6a94861d215b405424b128be49d91b5a0077da3f
Lucas Campos Amorim de Mendonça <lcamendonca@usp.br>
2022-01-28 18:13:22 -0300

    added a dynamic way of adding roles (still debugging)

--- Business/AuthenticationBusiness/IdentityInitializer.cs
+++ Business/AuthenticationBusiness/IdentityInitializer.cs
@@ -1,9 +1,11 @@
 ﻿using DataBase.Repository.Base;
+using Domain.Entities;
 using Domain.Entities.Security;
 using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -30,16 +32,30 @@ namespace Business.AuthenticationBusiness
 
         public void Initialize()
         {
-            if (!_roleManager.RoleExistsAsync(_tokenConfigurations.AccessRole).Result)
+            //List<IdentityResult> roles = new List<IdentityResult>();
+            foreach (PropertyInfo property in typeof(AccessRoles).GetProperties())
             {
-                var resultado = _roleManager.CreateAsync(
-                    new IdentityRole(_tokenConfigurations.AccessRole)).Result;
-                if (!resultado.Succeeded)
+                if (!_roleManager.RoleExistsAsync(property.Name).Result)
                 {
-                    throw new Exception(
-                        $"Erro durante a criação da role {_tokenConfigurations.AccessRole}.");
+                    //roles.Add(_roleManager.CreateAsync(new IdentityRole(property.Name)).Result);
+                    var result = _roleManager.CreateAsync(new IdentityRole(property.Name)).Result;
+                    if (!result.Succeeded)
+                    {
+                        throw new Exception($"Erro durante a criação da role {property.Name}.");
+                    }
                 }
             }
+
+            //if (!_roleManager.RoleExistsAsync(_tokenConfigurations.AccessRole).Result)
+            //{
+            //    var resultado = _roleManager.CreateAsync(
+            //        new IdentityRole(_tokenConfigurations.AccessRole)).Result;
+            //    if (!resultado.Succeeded)
+            //    {
+            //        throw new Exception(
+            //            $"Erro durante a criação da role {_tokenConfigurations.AccessRole}.");
+            //    }
+            //}
 
             var userInitial = _userManager.FindByNameAsync("admin").GetAwaiter().GetResult();
             if (userInitial == null)
@@ -53,7 +69,6 @@ namespace Business.AuthenticationBusiness
                 }, "@Admin123", _tokenConfigurations.AccessRole);
             }
         }
-
         private void CreateUser(
             AuthorizationUserDB user,
             string password,
