32ff6aa2879baae74acf6d72955acc8c050a7ec0
Lucas Campos Amorim de Mendonça <lcamendonca@usp.br>
2022-03-18 18:26:18 -0300

    checkpoint

---
+++ ProjetoDemo.BackgroundService/BackGroundServiceModules.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoDemo.BackgroundService
+{
+    public static class BackGroundServiceModules
+    {
+        public static void AddBackgroundServiceModule(this IServiceCollection services, IConfiguration configuration)
+        {
+
+        }
+    }
+}
