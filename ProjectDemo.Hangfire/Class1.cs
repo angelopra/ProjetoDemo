d419b159e1da0d9856a441e5c32713a7e6f572cc
Lucas Campos Amorim de Mendonça <lcamendonca@usp.br>
2022-03-18 16:34:34 -0300

    save

--- ProjectDemo.Hangfire/Class1.cs
+++ ProjectDemo.Hangfire/Class1.cs
@@ -1,8 +1,30 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Hangfire.SqlServer;
+using Hangfire;
+using Microsoft.Extensions.Configuration;
+using Domain.Interfaces;
 
-namespace ProjectDemo.Hangfire
+namespace ProjetoDemo.BackGroundService
 {
-    public class Class1
+    public static class HangfireModule
     {
+        public static void AddHangfireModule(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("HangfireConnection");
+
+            // Add Hangfire services.
+            services.AddHangfire(configuration =>
+            {
+                configuration.UseSqlServerStorage(connectionString);
+            });
+            // Add the processing server as IHostedService
+            services.AddHangfireServer();
+
+            services.AddSingleton<IProductSubscriber, ProductSubscriber>();
+        }
+
     }
 }
