6f533ed118edd0ad9a3be1133dae2d34ec77b85b
Lucas Campos Amorim de Mendonça <lcamendonca@usp.br>
2022-03-21 15:01:34 -0300

    saving some changes

---
+++ Business/ProductBusiness/Subscriber/ProductUpdateSubscriber.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Messengers;
+using Domain.Messengers.QueueType;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ProductBusiness.Subscriber
+{
+    public class ProductUpdateSubscriber : SubscriberManagerBase<ProductUpdateQueue>
+    {
+        public ProductUpdateSubscriber(ProducerConnection connection, ProductUpdateQueue obj) : base(connection, obj)
+        {
+        }
+
+        public override void ProcessEvent(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
